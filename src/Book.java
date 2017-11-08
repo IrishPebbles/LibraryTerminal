d6eea8bc54154fefe07fd6030eccdede58bac8a1
IrishPebbles <lena.l.hand@gmail.com>
2017-11-08 15:02:11 -0500

    getters and setters Book class

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,19 @@
 
 public class Book extends LibraryItem {
+	private String author = " ";
+	
+	
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
 }
