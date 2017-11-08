ad0a84f55660975ec3e4991af9a6d7ec22690e55
IrishPebbles <lena.l.hand@gmail.com>
2017-11-08 11:13:26 -0500

    initial commit

---
+++ src/BookApp.java
@@ -0,0 +1,9 @@
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Books!");
+
+	}
+
+}
---
+++ src/CheckedBook.java
@@ -0,0 +1,4 @@
+
+public class CheckedBook extends Book {
+
+}
