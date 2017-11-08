589984b472b94e9cb40f053ac7374be9fe7b479a
IrishPebbles <lena.l.hand@gmail.com>
2017-11-08 14:29:28 -0500

    Changed Classes and added stubs to Main

--- src/BookApp.java
+++ src/BookApp.java
@@ -5,6 +5,14 @@ public class BookApp {
 		System.out.println("Hello Books!");
 		System.out.println("Goodbye!");
 
+		// 1. User Menu with 6 options
+		
+		// 2. Class to decide based on options
+		
+		// 3. re-loop
+		
+		
+		// ?. Save back to text file
 	}
 
 }
